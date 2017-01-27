ffed320ab83ebc7a346ed1f65c905dca1620633c
Or-el Mousaffi <orel.mousaffi@gmail.com>
2017-01-27 17:27:58 -0500

    Generated token, started passport config.

--- app_api/controllers/main-api.js
+++ app_api/controllers/main-api.js
@@ -1,3 +1,11 @@
+var jwt = require('jsonwebtoken');
+
+function generateJWT() {
+  return jwt.sign({
+    exp: 300
+  }, 'ThisIsSecret');
+}
+
 module.exports.showAllUsers = function(req, res) {
-  res.render('allUsers', { title: "All users" });
+  res.render('allUsers', { title: "All users", token: generateJWT() });
 }
